afcf6dd377fc54a47a13f916ac5af94432321151
Guilherme Varandas <jgvarandas@gmail.com>
2019-01-22 13:37:39 -0200

    Updates

---
+++ precache-manifest.753ad54ed3eafa675cf18323a5ccb671.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec4699328b83638a9f02b2265020ea27",
+    "url": "/static/media/Guilherme.ec469932.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "35e7b7e7ee6d8f04d2f0",
+    "url": "/static/js/main.35e7b7e7.chunk.js"
+  },
+  {
+    "revision": "7fd07a8e490eccb6237f",
+    "url": "/static/js/1.7fd07a8e.chunk.js"
+  },
+  {
+    "revision": "5325fb39bc62ffb0d77786c0ff3c84ba",
+    "url": "/index.html"
+  }
+];
