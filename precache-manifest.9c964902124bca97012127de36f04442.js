8ad65409dbbf1fd59c44b68ba22083ac2e6efecb
Guilherme <jgvarandas@gmail.com>
2019-10-08 00:36:10 -0400

    Updates

---
+++ precache-manifest.9c964902124bca97012127de36f04442.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0764cedbc82b646972d5f9bee1823f04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2daf57a92d6f214e823e",
+    "url": "/static/js/2.f0d9a091.chunk.js"
+  },
+  {
+    "revision": "93e35c2d258256aa724f",
+    "url": "/static/js/3.d1d4a4f3.chunk.js"
+  },
+  {
+    "revision": "d2fbe7d62660f556cdbf",
+    "url": "/static/js/4.6884772a.chunk.js"
+  },
+  {
+    "revision": "d6f68d873588ca7b9b88",
+    "url": "/static/js/main.fd306729.chunk.js"
+  },
+  {
+    "revision": "b7cf8a4679e0840834e8",
+    "url": "/static/js/runtime-main.2e6fa1b5.js"
+  },
+  {
+    "revision": "ec4699328b83638a9f02b2265020ea27",
+    "url": "/static/media/Guilherme.ec469932.png"
+  }
+]);
