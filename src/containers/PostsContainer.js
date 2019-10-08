18484f473c1076c3044c3db72ead7026cbe70b76
Guilherme <jgvarandas@gmail.com>
2019-10-07 23:23:55 -0400

    Migrating component to use React Hooks

--- src/containers/PostsContainer.js
+++ src/containers/PostsContainer.js
@@ -1,43 +1,28 @@
 import React from 'react';
 
-const MEDIUM_API_ADDRESS = 'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@gvarandas';
+const MEDIUM_API_ADDRESS =
+  'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@gvarandas';
 
-// function replicate(element, number) {
-//   return new Array(number).fill(element[0]);
-// }
+const PostsContainer = ({ placeholder, children }) => {
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [posts, setPosts] = React.useState([]);
+  const fetchController = React.useRef(new AbortController());
 
-class PostsContainer extends React.Component {
-  state = {
-    isLoading: true,
-    posts: [],
-  };
-
-  fetchController = new AbortController();
-
-  componentDidMount() {
-    fetch(MEDIUM_API_ADDRESS, { signal: this.fetchController.signal })
+  React.useEffect(() => {
+    setIsLoading(true);
+    fetch(MEDIUM_API_ADDRESS, { signal: fetchController.current.signal })
       .then(response => response.json())
       .then(response => {
         const posts = response.items.filter(item => item.categories.length);
-        this.setState({
-          // posts: replicate(posts, 11),
-          posts: posts,
-          isLoading: false,
-        });
+        setPosts(posts);
+        setIsLoading(false);
       })
       .catch(error => console.log('error retrieving posts:', error));
-  }
 
-  componentWillUnmount() {
-    this.fetchController.abort();
-  }
+    return () => fetchController.current.abort();
+  }, []);
 
-  render() {
-    return this.state.isLoading ?
-      this.props.placeholder()
-      :
-      this.props.children(this.state.posts);
-  }
-}
+  return isLoading ? placeholder() : children(posts);
+};
 
 export default PostsContainer;
