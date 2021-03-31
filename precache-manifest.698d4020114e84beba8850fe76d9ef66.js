bc205a1845f04ac8c3f741cc7de928b7f55376cd
Guilherme Varandas <guil.varandas@shopify.com>
2021-03-31 14:00:18 -0400

    Updates

---
+++ precache-manifest.698d4020114e84beba8850fe76d9ef66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1139fa202d788268d1dfbad2e60e93e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "933395f96b2a4ec5f279",
+    "url": "/static/js/2.7b6512e3.chunk.js"
+  },
+  {
+    "revision": "d228254f4ea6be4858fc",
+    "url": "/static/js/3.552118b8.chunk.js"
+  },
+  {
+    "revision": "b8196a209a12b2a29962",
+    "url": "/static/js/main.d656f33f.chunk.js"
+  },
+  {
+    "revision": "210bc18bffe0cf563663",
+    "url": "/static/js/runtime-main.ff9de802.js"
+  },
+  {
+    "revision": "a2287def646d3052f31e0ee010f83286",
+    "url": "/static/media/Guilherme.a2287def.webp"
+  },
+  {
+    "revision": "ec4699328b83638a9f02b2265020ea27",
+    "url": "/static/media/Guilherme.ec469932.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8731e8b4b59e7efe72d6f79118b6efc4.js"
+  "/precache-manifest.698d4020114e84beba8850fe76d9ef66.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
